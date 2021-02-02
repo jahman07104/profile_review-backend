584e5c93df5056393c0eaa96b72d51c3013078af
jahman07104 <jahman@bellatlantic.net>
2021-02-02 17:42:10 -0500

    updated backend

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,40 @@
 class ProfilesController < ApplicationController
+    def index
+     # @profiles = Profile.all.order(:name)
+      @profiles = profile.all
+      render json: @profiles, status: 200
+    end
+  
+    def show 
+      @profile = Profile.find(params[:id])
+  
+      render json: @profile, status: 200
+    end
+  
+    def create
+      @profile = Profile.create(profile_params)
+  
+      render json:@profile, status: 200
+    end
+  
+    def update
+      @profile = Profile.find(params[:id])
+      @profile.update(profile_params)
+  
+      render json:@profile, status: 200
+    end
+  
+    def destroy
+      @profile = Profile.find(params[:id])
+      @profile.delete
+  
+      render json: {profileId: @profile.id}
+    end
+  
+    private
+    def Profile_params
+      params.require(:profile).permit(:name,:image,:email,:city,:phone)
+    end
+  end
+  
 end
